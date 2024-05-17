8b90584a06ba2d2dbae7aff531b102d6592775c2
himza25 <himza.of.theking95@gmail.com>
2024-05-16 20:31:30 -0700

    task 0 to 5

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,79 +1,89 @@
 #!/usr/bin/env python3
 """
-    RedactingFormatter
+This module provides functions to obfuscate log messages.
 """
-import csv
-import logging
-import os
+
 import re
-from typing import List
+import logging
+from typing import List, Tuple
+import os
 import mysql.connector
 
-PII_FIELDS = ("name", "email", "phone", "ssn", "password")
+
+PII_FIELDS: Tuple[str, ...] = ("name", "email", "phone", "ssn", "password")
+
+
+def filter_datum(fields: List[str], redaction: str, message: str,
+                 separator: str) -> str:
+    """Obfuscates fields in a log message."""
+    pattern = '|'.join([f'{field}=[^{separator}]*' for field in fields])
+    return re.sub(pattern, lambda m: f'{m.group(0).split("=")[0]}={redaction}',
+                  message)
 
 
 class RedactingFormatter(logging.Formatter):
-    """ Redacting Formatter class
-    """
+    """Redacting Formatter class"""
 
     REDACTION = "***"
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
     def __init__(self, fields: List[str]):
-        """ init """
-        super(RedactingFormatter, self).__init__(self.FORMAT)
+        super().__init__(self.FORMAT)
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
-        """ format record """
-        return filter_datum(self.fields, self.REDACTION,
-                            super(RedactingFormatter, self).format(record),
+        """Formats log record by obfuscating specified fields."""
+        original = super().format(record)
+        return filter_datum(self.fields, self.REDACTION, original,
                             self.SEPARATOR)
 
 
-def filter_datum(fields: List[str], redaction: str, message: str,
-                 separator: str) -> str:
-    """ returns the log message obfuscated """
-    return re.sub(r"(\w+)=([a-zA-Z0-9@\.\-\(\)\ \:\^\<\>\~\$\%\@\?\!\/]*)",
-                  lambda match: match.group(1) + "=" + redaction
-                  if match.group(1) in fields else match.group(0), message)
-
-
 def get_logger() -> logging.Logger:
-    """ return a logger object """
-    lg = logging.getLogger("user_data")
-    lg.setLevel(logging.INFO)
-    lg.propagate = False
-    sh = logging.StreamHandler()
-    sh.setFormatter(RedactingFormatter(PII_FIELDS))
-    lg.addHandler(sh)
-    return lg
+    """Creates and returns a logger object."""
+    logger = logging.getLogger("user_data")
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+
+    stream_handler = logging.StreamHandler()
+    formatter = RedactingFormatter(fields=PII_FIELDS)
+    stream_handler.setFormatter(formatter)
+
+    logger.addHandler(stream_handler)
+
+    return logger
 
 
-def get_db() -> mysql.connector.connection.MySQLConnection:
-    """ connect to MySQL database """
+def get_db():
+    """Connects to the secure database and returns a MySQLConnection object."""
+    username = os.getenv('PERSONAL_DATA_DB_USERNAME', 'root')
+    password = os.getenv('PERSONAL_DATA_DB_PASSWORD', 'root')
+    host = os.getenv('PERSONAL_DATA_DB_HOST', 'localhost')
+    db_name = os.getenv('PERSONAL_DATA_DB_NAME')
+
+    if db_name is None:
+        raise Exception("PERSONAL_DATA_DB_NAME environment variable not set")
+
     return mysql.connector.connect(
-        host=os.getenv("PERSONAL_DATA_DB_HOST", "root"),
-        database=os.getenv("PERSONAL_DATA_DB_NAME"),
-        user=os.getenv("PERSONAL_DATA_DB_USERNAME", "localhost"),
-        password=os.getenv("PERSONAL_DATA_DB_PASSWORD", ""),
+        user=username,
+        password=password,
+        host=host,
+        database=db_name
     )
 
 
-def main():
-    """
-    main function
-    """
-    con = get_db()
-    users = con.cursor()
-    users.execute("SELECT CONCAT('name=', name, ';ssn=', ssn, ';ip=', ip, \
-        ';user_agent', user_agent, ';') AS message FROM users;")
-    formatter = RedactingFormatter(fields=PII_FIELDS)
+def main() -> None:
+    """Obtains a DB connection and retrieves all rows in the users table."""
+    db = get_db()
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM users;")
     logger = get_logger()
-
-    for user in users:
-        logger.log(logging.INFO, user[0])
+    for row in cursor:
+        message = ("name={}; email={}; phone={}; ssn={}; password={};"
+                   " ip={}; last_login={}; user_agent={};").format(*row)
+        logger.info(message)
+    cursor.close()
+    db.close()
 
 
 if __name__ == "__main__":
