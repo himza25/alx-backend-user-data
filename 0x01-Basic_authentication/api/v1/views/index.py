007adc2c30afd09eddbdbe2c71c02918611f06ec
himza25 <himza.of.theking95@gmail.com>
2024-05-21 18:35:05 -0700

    task 1

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
-""" Module of Index views
+"""
+Module of Index views
 """
 from flask import jsonify, abort
 from api.v1.views import app_views
@@ -7,20 +8,19 @@ from api.v1.views import app_views
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status() -> str:
-    """ GET /api/v1/status
+    """
+    GET /api/v1/status
     Return:
       - the status of the API
     """
     return jsonify({"status": "OK"})
 
 
-@app_views.route('/stats/', strict_slashes=False)
-def stats() -> str:
-    """ GET /api/v1/stats
-    Return:
-      - the number of each objects
+@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
+def unauthorized() -> str:
     """
-    from models.user import User
-    stats = {}
-    stats['users'] = User.count()
-    return jsonify(stats)
+    GET /api/v1/unauthorized
+    Return:
+      - raises 401 error
+    """
+    abort(401)
