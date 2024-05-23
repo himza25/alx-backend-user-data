a5d68f650b1a969e53fd5365fcccfdeb1df4a458
himza25 <himza.of.theking95@gmail.com>
2024-05-23 10:52:42 -0700

    task 0

---
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+"""Auth module"""
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    """Auth class to manage API authentication"""
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """Checks if the path requires authentication"""
+        if path is None or excluded_paths is None or len(excluded_paths) == 0:
+            return True
+        if path[-1] != '/':
+            path += '/'
+        for ep in excluded_paths:
+            if ep.endswith('*'):
+                if path.startswith(ep[:-1]):
+                    return False
+            else:
+                if ep[-1] != '/':
+                    ep += '/'
+                if path == ep:
+                    return False
+        return True
+
+    def authorization_header(self, request=None) -> str:
+        """Retrieves the authorization header from the request"""
+        if request is None:
+            return None
+        return request.headers.get('Authorization')
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """Retrieves the current user from the request"""
+        return None
