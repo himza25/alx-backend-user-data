ec6ef2fb1df7f269a8c7ad623c0b717fa8c04f20
himza25 <himza.of.theking95@gmail.com>
2024-05-23 18:07:32 -0700

    task 8

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
-"""Session authentication module"""
-import uuid
+""" SessionAuth module for handling session authentication
+"""
 from api.v1.auth.auth import Auth
-from models.user import User
+import uuid
 
 
 class SessionAuth(Auth):
-    """Session authentication class"""
+    """ SessionAuth class for handling session authentication
+    """
     user_id_by_session_id = {}
 
     def create_session(self, user_id: str = None) -> str:
@@ -18,17 +19,20 @@ class SessionAuth(Auth):
         return session_id
 
     def user_id_for_session_id(self, session_id: str = None) -> str:
-        """Returns a user ID based on a session ID"""
+        """Returns a User ID based on a Session ID"""
         if session_id is None or not isinstance(session_id, str):
             return None
         return self.user_id_by_session_id.get(session_id)
 
-    def current_user(self, request=None):
-        """Returns a User instance based on a cookie value"""
+    def destroy_session(self, request=None) -> bool:
+        """Deletes the user session / logs out"""
+        if request is None:
+            return False
         session_id = self.session_cookie(request)
         if session_id is None:
-            return None
+            return False
         user_id = self.user_id_for_session_id(session_id)
         if user_id is None:
-            return None
-        return User.get(user_id)
+            return False
+        del self.user_id_by_session_id[session_id]
+        return True
