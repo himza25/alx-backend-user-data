308127676bc1ad1396cdacb58451d4ee1bf72c24
himza25 <himza.of.theking95@gmail.com>
2024-05-30 01:44:16 -0700

    task db

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -4,6 +4,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, Session
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
 from user import Base, User
 
 
@@ -11,7 +13,7 @@ class DB:
     """DB class"""
 
     def __init__(self) -> None:
-        """Initialize a new DB instance"""
+        """Initialize DB"""
         self._engine = create_engine("sqlite:///a.db", echo=False)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
@@ -19,23 +21,26 @@ class DB:
 
     @property
     def _session(self) -> Session:
-        """Memoized session object"""
+        """Session object"""
         if self.__session is None:
             DBSession = sessionmaker(bind=self._engine)
             self.__session = DBSession()
         return self.__session
 
     def add_user(self, email: str, hashed_password: str) -> User:
-        """
-        Add a new user to the database
-        Args:
-            email (str): The email of the user
-            hashed_password (str): The hashed password of the user
-        Returns:
-            User: The created User object
-        """
+        """Add user"""
         user = User(email=email, hashed_password=hashed_password)
         self._session.add(user)
         self._session.commit()
         self._session.refresh(user)
+        return user
+
+    def find_user_by(self, **kwargs) -> User:
+        """Find user by kwargs"""
+        try:
+            user = self._session.query(User).filter_by(**kwargs).one()
+        except NoResultFound:
+            raise NoResultFound("No user found")
+        except InvalidRequestError:
+            raise InvalidRequestError("Invalid query arguments")
         return user
