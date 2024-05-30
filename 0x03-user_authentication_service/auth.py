a4d7be81fe17b97a2ccabe4be6c11a4e2f06ff4a
himza25 <himza.of.theking95@gmail.com>
2024-05-30 01:50:16 -0700

    task auth

---
+++ 0x03-user_authentication_service/auth.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""Auth module
+"""
+import bcrypt
+from db import DB
+from user import User
+from sqlalchemy.orm.exc import NoResultFound
+
+
+def _hash_password(password: str) -> bytes:
+    """Hash a password using bcrypt"""
+    salt = bcrypt.gensalt()
+    hashed = bcrypt.hashpw(password.encode(), salt)
+    return hashed
+
+
+class Auth:
+    """Auth class to interact with the authentication database."""
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """Register a new user"""
+        try:
+            self._db.find_user_by(email=email)
+            raise ValueError(f"User {email} already exists")
+        except NoResultFound:
+            hashed_password = _hash_password(password)
+            user = self._db.add_user(email, hashed_password.decode('utf-8'))
+            return user
