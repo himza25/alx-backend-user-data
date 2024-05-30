95e4b9a6b7eb991740bc0b0320ce51491a2c16b5
himza25 <himza.of.theking95@gmail.com>
2024-05-30 02:04:27 -0700

    task auth

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -2,6 +2,7 @@
 """Auth module
 """
 import bcrypt
+import uuid
 from db import DB
 from user import User
 from sqlalchemy.orm.exc import NoResultFound
@@ -12,6 +13,11 @@ def _hash_password(password: str) -> bytes:
     salt = bcrypt.gensalt()
     hashed = bcrypt.hashpw(password.encode(), salt)
     return hashed
+
+
+def _generate_uuid() -> str:
+    """Generate a new UUID"""
+    return str(uuid.uuid4())
 
 
 class Auth:
@@ -29,3 +35,23 @@ class Auth:
             hashed_password = _hash_password(password)
             user = self._db.add_user(email, hashed_password.decode('utf-8'))
             return user
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """Validate user login credentials"""
+        try:
+            user = self._db.find_user_by(email=email)
+            return bcrypt.checkpw(
+                password.encode(), user.hashed_password.encode()
+            )
+        except NoResultFound:
+            return False
+
+    def create_session(self, email: str) -> str:
+        """Create a session ID for a user"""
+        try:
+            user = self._db.find_user_by(email=email)
+            session_id = _generate_uuid()
+            self._db.update_user(user.id, session_id=session_id)
+            return session_id
+        except NoResultFound:
+            return None
