f8f63c4387f922654aa4ce6c22d3f34f18dba5f3
himza25 <himza.of.theking95@gmail.com>
2024-05-30 01:54:19 -0700

    tasks app

---
+++ 0x03-user_authentication_service/app.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""Basic Flask app"""
+from flask import Flask, jsonify, request
+from auth import Auth
+
+app = Flask(__name__)
+AUTH = Auth()
+
+
+@app.route("/", methods=["GET"])
+def welcome():
+    """Welcome route"""
+    return jsonify({"message": "Bienvenue"})
+
+
+@app.route("/users", methods=["POST"])
+def users():
+    """Register user route"""
+    email = request.form.get("email")
+    password = request.form.get("password")
+
+    try:
+        user = AUTH.register_user(email, password)
+        return jsonify({"email": user.email, "message": "user created"}), 200
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
